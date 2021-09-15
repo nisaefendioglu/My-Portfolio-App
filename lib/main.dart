04448139328fbad27a4d2644c2f01f67b99924b2
nisaefendioglu <nisaefendioglu0@gmail.com>
2021-09-15 20:16:10 +0300

    ui added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 void main() {
   runApp(const MyApp());
@@ -6,110 +7,193 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Nisa's Portfolio",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MyHomePage extends StatelessWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Color(0xFFFF559F),
+                  Color(0xFFCF5CCF),
+                  Color(0xFFFF57AC),
+                  Color(0xFFFF6D91),
+                  Color(0xFFFF8D7E),
+                  Color(0xFFB6BAA6),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+              padding: const EdgeInsets.all(110.0),
+              child: CircleAvatar(
+                radius: 80,
+                backgroundColor: Color(0xFFb936b9),
+                backgroundImage: AssetImage("assets/me.png"),
+              )),
+          Center(
+            child: Text(
+              "Hi There 👋 \nMy name is Nisa Efendioğlu, \nI’m Software Developer. ",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(60.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                SnakeButton(
+                  child: Text("Hello World!"),
+                  onTap: () {
+                    print("on tap");
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class SnakeButton extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+  final Color snakeColor;
+  final Color borderColor;
+  final double borderWidth;
+  final VoidCallback onTap;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  const SnakeButton({
+    Key? key,
+    required this.child,
+    this.duration = const Duration(milliseconds: 1500),
+    this.borderWidth = 4.0,
+    this.borderColor = Colors.white,
+    this.snakeColor = Colors.purple,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  _SnakeButtonState createState() => _SnakeButtonState();
+}
+
+class _SnakeButtonState extends State<SnakeButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this,
+        duration: widget.duration ??
+            const Duration(
+              milliseconds: 1500,
+            ));
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return InkWell(
+      onTap: widget.onTap,
+      child: CustomPaint(
+        painter: _SnakePainter(
+          animation: _controller,
+          borderColor: widget.borderColor,
+          borderWidth: widget.borderWidth,
+          snakeColor: widget.snakeColor,
+        ),
+        child: Container(
+          alignment: Alignment.center,
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: widget.child,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class _SnakePainter extends CustomPainter {
+  final Animation animation;
+  final Color snakeColor;
+  final Color borderColor;
+  final double borderWidth;
+  _SnakePainter({
+    required this.animation,
+    required this.snakeColor,
+    required this.borderColor,
+    required this.borderWidth,
+  }) : super(
+          repaint: animation,
+        );
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final rect = Offset.zero & size;
+    final paint = Paint()
+      ..shader = SweepGradient(
+        colors: [
+          snakeColor,
+          Colors.transparent,
+        ],
+        stops: [
+          0.7,
+          1.0,
+        ],
+        startAngle: 0.0,
+        endAngle: vector.radians(180),
+        transform: GradientRotation(vector.radians(360.0 * animation.value)),
+      ).createShader(rect);
+
+    final path = Path.combine(
+      PathOperation.xor,
+      Path()..addRect(rect),
+      Path()
+        ..addRect(
+          rect.deflate(borderWidth),
+        ),
+    );
+    canvas.drawRect(
+      rect.deflate(borderWidth / 2),
+      Paint()
+        ..color = borderColor
+        ..strokeWidth = borderWidth
+        ..style = PaintingStyle.stroke,
+    );
+    path.addRect(rect);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
